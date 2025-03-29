5fddb507d98fc076455a1badbbdf602ec3e80824
Grey Bunny <iamgreybunny@gmail.com>
2025-03-29 14:02:30 +0800

    Revert "Added:"
    This reverts commit 256ec03c900f5c72bbe2d88340acc8ab5aeb0268.

--- MainApp.java
+++ MainApp.java
@@ -1,10 +1,4 @@
-import login.LoginView;
-
 public class MainApp {
     public static void main(String[] args){
-
-        // Prompts Login
-        LoginView loginView = new LoginView();
-        loginView.promptLogin();
     }
 }

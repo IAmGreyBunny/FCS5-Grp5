fc50fd44f22dc24a5944964b2f45d649cd267381
Grey Bunny <iamgreybunny@gmail.com>
2025-03-29 14:07:11 +0800

    Revert changes

---
+++ login/LoginController.java
@@ -0,0 +1,12 @@
+package login;
+
+import User.User;
+
+public class LoginController {
+    public static User login(String username, String password)
+    {
+        User user = new User();
+
+        return user;
+    }
+}

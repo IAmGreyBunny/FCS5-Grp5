6a089578512edf16e2b3b6cb99f6c894f907f260
Grey Bunny <iamgreybunny@gmail.com>
2025-03-30 22:05:10 +0800

    Added: 1) Singleton implementation of GlobalData class 2) Added empty loadInitialData() to ProgramState Class

--- data/GlobalData.java
+++ data/GlobalData.java
@@ -5,5 +5,21 @@ import user.User;
 import java.util.ArrayList;
 
 public class GlobalData {
+    private static GlobalData globalData = null; // References itself
+
     private static ArrayList<User> listOfUsers;
+
+    private GlobalData() //Private constructor to prevent creation of this class in other places
+    {
+
+    }
+
+    public static GlobalData getGlobalData() {
+        if(globalData == null) //Checks if global data exists
+        {
+            globalData = new GlobalData();
+        }
+
+        return globalData;
+    }
 }
--- programstate/ProgramState.java
+++ programstate/ProgramState.java
@@ -1,7 +1,15 @@
 package programstate;
 
+import data.GlobalData;
 import user.CurrentUser;
+
+import java.io.File;
 
 public class ProgramState {
     private CurrentUser currentUser = null;
+    private GlobalData globalData = GlobalData.getGlobalData();
+
+    public void loadInitialData()
+    {
+    }
 }

a2fa1c33e2185d7e562875312effb844ad1ea999
Grey Bunny <iamgreybunny@gmail.com>
2025-04-03 13:25:00 +0800

    Updated: 1) BTOProject 2) BTOUnitType

--- btoproject/BTOProject.java
+++ btoproject/BTOProject.java
@@ -1,24 +1,20 @@
 package btoproject;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class BTOProject {
     private String projectName;
     private String location;
-    private int totalUnits;
-    private double pricePerUnit;
+    private ArrayList<BTOUnitType> listOfUnits;
     private LocalDate estimatedCompletionDate;
 
-    // Constructor
-    public BTOProject(String projectName, String location, int totalUnits, double pricePerUnit, LocalDate estimatedCompletionDate) {
+    public BTOProject(String projectName, String location, LocalDate estimatedCompletionDate) {
         this.projectName = projectName;
         this.location = location;
-        this.totalUnits = totalUnits;
-        this.pricePerUnit = pricePerUnit;
         this.estimatedCompletionDate = estimatedCompletionDate;
     }
 
-    // Getters and Setters
     public String getProjectName() {
         return projectName;
     }
@@ -35,20 +31,13 @@ public class BTOProject {
         this.location = location;
     }
 
-    public int getTotalUnits() {
-        return totalUnits;
+    public ArrayList<BTOUnitType> getListOfUnits() {
+        return listOfUnits;
     }
 
-    public void setTotalUnits(int totalUnits) {
-        this.totalUnits = totalUnits;
-    }
-
-    public double getPricePerUnit() {
-        return pricePerUnit;
-    }
-
-    public void setPricePerUnit(double pricePerUnit) {
-        this.pricePerUnit = pricePerUnit;
+    public void addUnitType(String name, int available, int total, double pricePerUnit) {
+        BTOUnitType btoUnitType = new BTOUnitType(name, available, total, pricePerUnit);
+        this.listOfUnits.add(btoUnitType);
     }
 
     public LocalDate getEstimatedCompletionDate() {
@@ -59,17 +48,16 @@ public class BTOProject {
         this.estimatedCompletionDate = estimatedCompletionDate;
     }
 
-    // Method to calculate total project value
+    /*
     public double calculateTotalValue() {
         return totalUnits * pricePerUnit;
-    }
+    }*/
 
-    // Method to check if project is completed
     public boolean isCompleted() {
         return LocalDate.now().isAfter(estimatedCompletionDate);
     }
 
-    // toString Method
+    /*
     @Override
     public String toString() {
         return "BTOProject{" +
@@ -80,4 +68,5 @@ public class BTOProject {
                 ", estimatedCompletionDate=" + estimatedCompletionDate +
                 '}';
     }
+    */
 }

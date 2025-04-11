e7f4a13de563af1b780af62e911e2fde7181e03d
Grey Bunny <iamgreybunny@gmail.com>
2025-04-11 22:02:21 +0800

    Changed: 1) Made Session a singleton class
    Removed:
    1) Extra LoginView

--- session/Session.java
+++ session/Session.java
@@ -5,11 +5,24 @@ import user.User;
 import view.View;
 
 public class Session {
+    private static Session session = null;
+
     private User currentUser = null;
     private View currentView = null;
     private GlobalData globalData = GlobalData.getGlobalData();
 
+    private Session() {
+
+    }
+
     public void loadInitialData() {
+    }
+
+    public static Session getSession() {
+        if (session == null) {
+            session = new Session();
+        }
+        return session;
     }
 
     public User getCurrentUser() {

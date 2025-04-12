8e7dd34af09960dde75eec734d6fddfed37a9ee2
efilyssem <efilyssem888@gmail.com>
2025-04-12 18:11:51 +0800

    Updated ListingController but still not working yet

--- btoproject/ListingController.java
+++ btoproject/ListingController.java
@@ -1,31 +1,31 @@
 package btoproject;
 
 import java.util.HashMap;
-
+import java.time.LocalDate;
 
 //public class ListingController {
-//    public static void login(HashMap<String, Object> userInput) {
-//        String name = userInput.get("name");
-//        String location = userInput.get("location");
-//        LocalDate estimatedCompletionDate = userInput.get("estimatedCompletionDate");
+//    public static void listing(HashMap<String, Object> userInput) {
+//        String name = (String) userInput.get("name");
+//        String location = (String) userInput.get("location");
+//        LocalDate estimatedCompletionDate = (LocalDate) userInput.get("estimatedCompletionDate");
 //        /*LocalDate openingDate = userInput.get("openingDate");
 //        LocalDate closingDate = userInput.get("closingDate"); */
-//        int officerSlots = userInput.get("officerSlots");
+//        int officerSlots = (int) userInput.get("officerSlots");
 //        BTOProject btoProject = new BTOProject(name, location, estimatedCompletionDate);
 //
-//        Object twoRoom = userInput.get("2-room");
+//        HashMap<String, Object> twoRoom = (HashMap<String,Object>) userInput.get("2-room");
 //        if (twoRoom != null) {
-//            int total = twoRoom.get("total");
-//            int available = twoRoom.get("available");
-//            double pricePerUnit = twoRoom.get("pricePerUnit");
-//            btoProject.addUnitType(twoRoom, available, total, pricePerUnit);
+//            int total = (int) twoRoom.get("total");
+//            int available = (int) twoRoom.get("available");
+//            double pricePerUnit = (double) twoRoom.get("pricePerUnit");
+//            btoProject.addUnitType("2-room", available, total, pricePerUnit);
 //        }
-//        Object threeRoom = userInput.get("3-room");
+//        HashMap<String,Object> threeRoom = (HashMap<String,Object>) userInput.get("3-room");
 //        if (threeRoom != null) {
-//            int total = threeRoom.get("total");
-//            int available = threeRoom.get("available");
-//            double pricePerUnit = threeRoom.get("pricePerUnit");
-//            btoProject.addUnitType(threeRoom, available, total, pricePerUnit);
+//            int total = (int) threeRoom.get("total");
+//            int available = (int) threeRoom.get("available");
+//            double pricePerUnit = (double) threeRoom.get("pricePerUnit");
+//            btoProject.addUnitType("3-room", available, total, pricePerUnit);
 //        }
 //
 //        System.out.println("Project successfully created!");

24e3b770c443b1f19c2f99f98162d3d7be6e2fe4
Grey Bunny <iamgreybunny@gmail.com>
2025-04-18 21:26:39 +0800

    Changes: 1) Added code to check database for username and password

--- login/AuthController.java
+++ login/AuthController.java
@@ -1,13 +1,24 @@
 package login;
 
+import session.Session;
+import user.UserRepository;
+
+import java.io.IOException;
 import java.util.HashMap;
 
 public class AuthController {
-    public static void authenticate(HashMap<String,String> userInput)
-    {
+    public static void authenticate(HashMap<String,String> userInput){
         //Checks userInput
 
+
         //Authenticate
+        try{
+            Session.getSession().setCurrentUser(
+                    UserRepository.findUserByLogin(userInput.get("username"),userInput.get("password"))
+            );
+        }catch (Exception e){
+
+        }
 
         //Update View
 

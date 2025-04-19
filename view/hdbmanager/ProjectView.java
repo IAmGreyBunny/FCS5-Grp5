6a1c4d48696418b72af73541882c83c1f29d4d2d
efilyssem <efilyssem888@gmail.com>
2025-04-19 15:30:49 +0800

    Update ProjectView.java

--- view/hdbmanager/ProjectView.java
+++ view/hdbmanager/ProjectView.java
@@ -3,18 +3,18 @@ package view.hdbmanager;
 import session.Session;
 import view.MenuView;
 import btoproject.BTOProject;
+import view.hdbmanager.HDBManagerHomeView;
+import user.hdbmanager.HDBProjectController;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.time.LocalDate;
 
 public class ProjectView extends MenuView {
     private int userInput;
-    private List<BTOProject> btoProject = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-
+    private HDBProjectController controller = new HDBProjectController();
 
     @Override
     public void show() {
@@ -34,15 +34,10 @@ public class ProjectView extends MenuView {
                     // code to create new listing (CreateListingForm)
                     break;
                 case 2:
-                    System.out.println("=== Edit Listing ===");
-                    printProjects();
-                    System.out.println("Enter project to be edited: ");
-                    // code to edit existing listing
+                    handleEditing();
                     break;
                 case 3:
-                    printProjects();
-                    System.out.println("Enter project to be deleted: ");
-                    // code to delete listing
+                    handleDeletion();
                     break;
                 case 4:
                     toggleVisibility();
@@ -60,71 +55,150 @@ public class ProjectView extends MenuView {
         }
     }
 
-    public void printProjects() {
-        for (BTOProject project : btoProject) {
-            System.out.println(project.getProjectName() + " -- " + project.getVisibility());
+    private void handleEditing() {
+        System.out.println("=== Edit Listing ===");
+        printProjects();
+        System.out.println("Enter project name to be edited: ");
+        String projectName = scanner.next();
+        List<BTOProject> projects = controller.getAllProjects();
+        BTOProject target = projects.stream().filter(p -> p.getProjectName().equalsIgnoreCase(projectName)).findFirst().orElse(null);
+        if (target != null) {
+            System.out.println("-- Current Details --");
+            System.out.println("1. Project Name: " + target.getProjectName());
+            System.out.println("2. Neighbourhood: " + target.getNeighbourhood());
+            /*  TODO option to print the types of flats available
+            System.out.println("3. Types of Flats: ");
+                TODO portion to specify which flat to edit
+            System.out.println("4. Choose the type of flats to edit");
+            */
+            System.out.println("5. Opening Date: " + target.getApplicationOpeningDate());
+            System.out.println("6. Closing Date " + target.getApplicationClosingDate());
+            System.out.println("7. Officer Slots: " + target.getOfficerSlots());
+            System.out.println("\nEnter portion to edit: ");
+            int portion = scanner.nextInt();
+            Object newValue = null;
+            try {
+                switch (field) {
+                    case 1 -> {
+                        System.out.println("Enter new project name: ");
+                        newValue = scanner.next();
+                    }
+                    case 2 -> {
+                        System.out.println("Enter new neighbourhood: ");
+                        newValue = scanner.next();
+                    }
+                    /* TODO case 3 and 4
+                    case 3 -> {
+                        
+                    }
+                    case 4 -> {
+
+                    } */
+                    case 5,6 -> {
+                        System.out.println("Enter new date (yyyy-mm-dd): ");
+                        newValue = LocalDate.parse(scanner.next());
+                    }
+                    case 7 -> {
+                        System.out.println("Enter new number of slots: ");
+                        newValue = scanner.nextInt();
+                    }
+                    default -> {
+                        System.out.println("Invalid selection.");
+                        return;
+                    }
+                }
+                if (controller.editListing(target, choice, newValue)) {
+                    System.out.println("Successfully edited!");
+                }
+                else {System.out.println("Failed to update.");}
+            } catch (Exception e) {
+                System.out.println("Project not found.");
+            }
         }
     }
 
-    public void toggleVisibility() {
-        System.out.println("==== Toggle Visibility ====\n");
-        printProjects();
+    private void handleDeletion() {
+        System.out.println("=== Delete Listing ===");
+        printProject();
+        System.out.println("Enter project to be deleted: ");
+        String delete = scanner.next();
+        if (controller.deleteProject(delete)) {
+            System.out.println("Project deleted.");
+        } else {System.out.println("Project not found");}
+    }
 
-        String bto;
+    public void toggleVisibility() {
+        System.out.println("=== Toggle Visibility ===");
+        printProject();
+
+        String projectName;
         BTOProject foundProject = null;
+        List<BTOProject> projects = controller.getAllProjects();
+
         do {
-            System.out.println("\nEnter Project Name: ");
-            bto = scanner.next();
-            for (BTOProject project : btoProject) {
-                if (project.getProjectName().equalsIgnoreCase(bto)) {
+            System.out.println("Enter project name: ");
+            projectName = scanner.next();
+            for (BTOProject project : projects) {
+                if (project.getProjectName().equalsIgnoreCase(projectName)) {
                     foundProject = project;
+                    System.out.println("Current visibility: " + foundProject.getVisibility());
                     break;
                 }
             }
             if (foundProject == null) {
-                System.out.println("Project not found!");
+                System.out.println("Project not found");
             }
         } while (foundProject == null);
-        
+
         String visibility;
         do {
             System.out.println("Enter new visibility (on/off): ");
             visibility = scanner.next();
         } while (!visibility.equalsIgnoreCase("on") && !visibility.equalsIgnoreCase("off"));
 
-        // to set the visibility of the specified project
+        boolean visible = visibility.equalsIgnoreCase("on");
+        if (controller.toggleVisibility(foundProject, visible)) {
+            System.out.println("Successfully toggled.");
+        } else {System.out.println("Toggle unsuccessful.");}
     }
-    
+
+    public void printProject() {
+        List<BTOProject> projects = controller.getAllProjects();
+        if (projects.isEmpty()) {
+            System.out.println("No projects available!");
+            return;
+        }
+        
+        for (BTOProject project : projects) {
+            System.out.println(project.getProjectName());
+        }
+    }
+
     public void viewProjects() {
-        System.out.println("==== View Project Listings ====");
+        System.out.println("===== View Project Listings ===");
         System.out.println("1. View All Projects");
         System.out.println("2. View My Projects");
 
         int choice = scanner.nextInt();
-        if (btoProject.isEmpty()) {
-            System.out.println("No projects created!");
-            return;
-        }
         switch (choice) {
-            case 1:
-                System.out.println("== All Projects ==");
-                btoProject.stream()
-                    .forEach(/* to print the details of each project */);
-                break;
-            case 2:
-                String managerNric = Session.getSession().getCurrentUser().getNric();
-                System.out.println("== My Projects ==");
-                List<BTOProject> myProjects = btoProject.stream()
-                    .filter(project -> project.getManager().getNric().equals(managerNric))
-                    .collect(Collectors.toList());
-
+            case 1 -> {
+                System.out.println("=== All Projects");
+                List<BTOProject> projects = controller.getAllProjects;
+                for (BTOProject project : projects) {
+                    System.out.println("project.getProjectName()");
+                }
+            }
+            case 2 -> {
+                String managerId = Session.getSession().getCurrentUser().getUid();
+                List<BTOProject> myProjects = controller.getMyProjects(managerId);
                 if (myProjects.isEmpty()) {
-                    System.out.println("You have not created any projects.");
+                    System.out.println("No projects created.");
                 }
                 else {
-                    // code to print the details of projects in myProjects
+                    myProjects.forEach(p -> System.out.println(p.getProjectName()));
                 }
-            default: System.out.println("Invalid choice!");
+            }
+            default -> System.out.println("Invalid choice!");
         }
     }
 }

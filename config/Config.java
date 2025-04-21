09336312ab052ea6335d53ca1b740be875ed2466
Grey Bunny <iamgreybunny@gmail.com>
2025-04-21 18:30:01 +0800

    Changed:
    1) condensed ProjectDetails with ProjectApplicationDetails and updated filepath in Config.java

--- config/Config.java
+++ config/Config.java
@@ -4,8 +4,7 @@ import java.util.Map;
 
 public class Config {
     public static final Map<String, String> filepath = Map.ofEntries(
-            Map.entry("Project","data/project/Project.xlsx"),
-            Map.entry("ProjectApplicationDetails","data/project/ProjectApplicationDetails.xlsx"),
+            Map.entry("ProjectDetails","data/project/ProjectDetails.xlsx"),
             Map.entry("ProjectManager","data/project/ProjectManager.xlsx"),
             Map.entry("ProjectOfficer","data/project/ProjectOfficer.xlsx"),
             Map.entry("UnitType","data/project/UnitType.xlsx"),

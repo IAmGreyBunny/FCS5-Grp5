2fa7b191008a3a4bee23e6e13b5713d7ff8f9eda
Grey Bunny <iamgreybunny@gmail.com>
2025-04-23 20:35:17 +0800

    Added: 1) getProjectById()
    Fixed:
    1) Date time format

--- project/ProjectRepository.java
+++ project/ProjectRepository.java
@@ -28,7 +28,7 @@ public class ProjectRepository {
                 int projectId = (int) row.getCell(0).getNumericCellValue();
                 String projectName = row.getCell(1).getStringCellValue();
                 String neighbourhood = row.getCell(2).getStringCellValue();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
                 LocalDate openingDate = LocalDate.parse(row.getCell(3).getStringCellValue(),formatter);
                 LocalDate closingDate = LocalDate.parse(row.getCell(4).getStringCellValue(),formatter);
                 int officerSlots = (int) row.getCell(5).getNumericCellValue();
@@ -44,6 +44,38 @@ public class ProjectRepository {
         }
 
         return listOfProjects;
+    }
+
+    public static Project getProjectById(int id){
+        try (
+                FileInputStream file = new FileInputStream(new File(Config.filepath.get("ProjectDetails")));
+                Workbook workbook = WorkbookFactory.create(file)
+        ) {
+            Sheet sheet = workbook.getSheetAt(0);
+
+            // Go through row in the Excel sheet
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+
+                if((int)row.getCell(0).getNumericCellValue() == id){
+                    int projectId = (int) row.getCell(0).getNumericCellValue();
+                    String projectName = row.getCell(1).getStringCellValue();
+                    String neighbourhood = row.getCell(2).getStringCellValue();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    LocalDate openingDate = LocalDate.parse(row.getCell(3).getStringCellValue(),formatter);
+                    LocalDate closingDate = LocalDate.parse(row.getCell(4).getStringCellValue(),formatter);
+                    int officerSlots = (int) row.getCell(5).getNumericCellValue();
+                    boolean visibility = row.getCell(6).getBooleanCellValue();
+
+                    Project project = new Project(projectId,projectName,neighbourhood,openingDate,closingDate,officerSlots,visibility);
+                    return project;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: ");
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public ArrayList<Integer> getProjectOfficersId(int projectId){

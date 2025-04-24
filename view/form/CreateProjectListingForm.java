b09d69d601179148f8cce5715d1ca779c1f7c6c3
efilyssem <efilyssem888@gmail.com>
2025-04-25 04:55:16 +0800

    update forms to create listing and unit

--- view/form/CreateProjectListingForm.java
+++ view/form/CreateProjectListingForm.java
@@ -1,11 +1,20 @@
 package view.form;
 
+import project.ProjectController;
+import project.ProjectRepository;
 import session.Session;
 import view.FormView;
+import validator.InputValidator;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CreateProjectListingForm extends FormView {
+
+    HashMap<String, Object> userInput = new HashMap<>();
+
     @Override
     public void prompt() {
         // get all the user input into variables(use the InputValidator functions to validate)
@@ -14,7 +23,72 @@ public class CreateProjectListingForm extends FormView {
         // createListingWithUserInput(HashMap<String, Object> userInput)
         // update currentView with new CreateUnitTypeForm()
 
-        //Session.getSession().setCurrentView(new CreateUnitTypeForm(projectId));
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String name;
+        String neighbourhood;
+        LocalDate openingDate;
+        LocalDate closingDate;
+        int officerSlots;
+
+        System.out.println("--- Create BTO Project ---");
+
+        System.out.println("Enter name of new project: ");
+        name = scanner.next();
+
+        System.out.println("Enter neighbourhood of this project: ");
+        neighbourhood = scanner.next();
+
+        System.out.println("Enter application opening date: ");
+        boolean validOpening;
+        String sOpeningDate;
+        do {
+            sOpeningDate = scanner.next();
+            validOpening = InputValidator.validateDate(sOpeningDate, "dd/MM/yyyy");
+            if (!validOpening) {
+                System.out.println("Invalid date format. Please enter the date in dd/MM/yyyy format.");
+            }
+        } while (!validOpening);
+        openingDate = LocalDate.parse(sOpeningDate, formatter);
+
+        System.out.println("Enter application closing date: ");
+        boolean validClosing;
+        String sClosingDate;
+        do {
+            sClosingDate = scanner.next();
+            validClosing = InputValidator.validateDate(sClosingDate, "dd/MM/yyyy");
+            if (!validClosing) {
+                System.out.println("Invalid date format. Please enter the date in dd/MM/yyyy format.");
+            }
+        } while (!validClosing);
+        closingDate = LocalDate.parse(sClosingDate, formatter);
+
+        System.out.println("Enter number of officer slots: ");
+        boolean validSlots;
+        String sSlots;
+        do {
+            sSlots = scanner.next();
+            validSlots = InputValidator.validateIntRange(sSlots, 1, 10);
+            if (!validSlots) {
+                System.out.println("Invalid input. Please enter a value between 1 and 10.");
+            }
+        } while (!validSlots);
+        officerSlots = Integer.parseInt(sSlots);
+
+
+        userInput.put("name", name);
+        userInput.put("neighbourhood", neighbourhood);
+        userInput.put("openingDate", openingDate);
+        userInput.put("closingDate", closingDate);
+        userInput.put("officerSlots", officerSlots);
+
+        ProjectController.createListingWithUserInput(this.getUserInput());
+
+        // to get the new project's id to create the units -- this is current the only way I can think of to get the id :(
+        int projectId = ProjectRepository.findMaxProjectId();
+
+        Session.getSession().setCurrentView(new CreateUnitTypeForm(projectId));
     }
 
     @Override
--- view/form/CreateUnitTypeForm.java
+++ view/form/CreateUnitTypeForm.java
@@ -1,14 +1,24 @@
 package view.form;
 
+import project.ProjectController;
+import session.Session;
 import view.FormView;
+import validator.InputValidator;
+import view.hdbmanager.ManagerProjectManagementView;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CreateUnitTypeForm extends FormView {
 
-    CreateUnitTypeForm(int projectId){
+    int projectId;
 
+    CreateUnitTypeForm(int projectId){
+        this.projectId = projectId;
     }
+
+    HashMap<String, Object> userInput = new HashMap<>();
+    Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
     @Override
     public void prompt() {
@@ -17,6 +27,76 @@ public class CreateUnitTypeForm extends FormView {
         // put all the user inputs into userInput based on the keys found in controller(exact string match)
         // createUnitTypeWithUserInput(HashMap<String, Object> userInput)
         // update currentView with new CreateUnitTypeForm()
+
+        String name;
+        int available;
+        int total;
+        double pricePerUnit;
+        boolean newUnit;     // to create more than 1 unit type
+
+
+        do {
+            System.out.println("Enter name of unit type: ");
+            name = scanner.next();
+
+            System.out.println("Enter total number of units: ");
+            String sTotal;
+            boolean validTotal;
+            do {
+                sTotal = scanner.next();
+                validTotal = InputValidator.validateIntRange(sTotal, 0, null);
+                if (!validTotal) {
+                    System.out.println("Invalid input. Please enter a non-negative integer.");
+                }
+            } while (!validTotal);
+            total = Integer.parseInt(sTotal);
+
+
+            System.out.println("Enter available number of units: ");
+            String sAvail;
+            boolean validAvail;
+            do {
+                sAvail = scanner.next();
+                validAvail = InputValidator.validateIntRange(sAvail, 0, total);
+                if (!validAvail) {
+                    System.out.println("Invalid input. Please enter a value between 0 and " + total + ".");
+                }
+            } while (!validAvail);
+            available = Integer.parseInt(sAvail);
+
+            System.out.println("Enter price per unit: ");
+            String sPrice;
+            boolean validPrice;
+            do {
+                sPrice = scanner.next();
+                validPrice = InputValidator.validatePositiveDouble(sPrice);
+                if (!validPrice) {
+                    System.out.println("Invalid input. Please enter a positive double value.");
+                }
+            } while (!validPrice);
+            pricePerUnit = Double.parseDouble(sPrice);
+
+            userInput.put("name", name);
+            userInput.put("available", available);
+            userInput.put("total", total);
+            userInput.put("pricePerUnit", pricePerUnit);
+
+            ProjectController.createUnitTypeWithUserInput(this.projectId, this.getUserInput());
+
+            System.out.println("Add another unit type (Y/N)?");
+            String input;
+            do {
+                input = scanner.next().toUpperCase();
+                if (!InputValidator.validateYesNo(input)) {
+                    System.out.println("Enter either 'Y' or 'N'.");
+                }
+            } while(!InputValidator.validateYesNo(input));
+
+            newUnit = input.equalsIgnoreCase("Y");
+
+        } while(newUnit);
+
+        Session.getSession().setCurrentView(new ManagerProjectManagementView());
     }
 
     @Override

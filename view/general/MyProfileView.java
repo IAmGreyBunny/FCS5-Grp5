6e75f088ee7a2127fb47d4e135e164951ee68ed2
Grey Bunny <iamgreybunny@gmail.com>
2025-04-24 18:46:40 +0800

    Changed: 1) Added UI for MyProfileView

--- view/general/MyProfileView.java
+++ view/general/MyProfileView.java
@@ -2,9 +2,27 @@ package view.general;
 
 import view.MenuView;
 
+import java.util.Scanner;
+
 public class MyProfileView extends MenuView {
+
+    int userInput;
+    Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
     @Override
     public void show() {
+        System.out.println("--- My Profile ---");
+        System.out.println("1) Change Password");
 
+        userInput = scanner.nextInt();
+
+        switch (userInput){
+            case 1:
+                // Change Password
+                break;
+            default:
+                System.out.println("Invalid Option");
+
+        }
     }
 }

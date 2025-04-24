e5f6def3dbefd6625eeea94ef4b5912ccf39ad23
Grey Bunny <iamgreybunny@gmail.com>
2025-04-24 18:41:59 +0800

    Changed: 1) Added Navigation to MyProfileView from HomeViews

---
+++ view/general/MyProfileView.java
@@ -0,0 +1,10 @@
+package view.general;
+
+import view.MenuView;
+
+public class MyProfileView extends MenuView {
+    @Override
+    public void show() {
+
+    }
+}
--- view/hdbofficer/HDBOfficerHomeView.java
+++ view/hdbofficer/HDBOfficerHomeView.java
@@ -4,6 +4,7 @@ import project.Project;
 import session.Session;
 import view.MenuView;
 import view.general.DefaultHomeView;
+import view.general.MyProfileView;
 
 import java.util.InputMismatchException;
 import java.util.List;
@@ -16,9 +17,15 @@ public class HDBOfficerHomeView extends MenuView {
     @Override
     public void show() {
         System.out.println("Welcome!");
-        System.out.println("1) Apply for BTO Project");
-        System.out.println("2) HDB Officer Registration");
-        System.out.println("2) Manage Project");
+        System.out.println("1) My Profile");
+        System.out.println("2) Apply for BTO Project");
+        System.out.println("3) View ALL projects");
+        System.out.println("4) View Applied BTO Projects");
+        System.out.println("5) Book Flat");
+        System.out.println("6) Request Withdrawal");
+        System.out.println("7) Enquiry");
+        System.out.println("8) HDB Officer Registration");
+        System.out.println("9) Manage Project");
 
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         try {
@@ -26,27 +33,30 @@ public class HDBOfficerHomeView extends MenuView {
 
             switch (userInput) {
                 case 1:
+                    // MyProfileView
+                    Session.getSession().setCurrentView(new MyProfileView());
+                case 2:
                     // BTOApplicationView
                     break;
-                case 2:
+                case 3:
                     // ProjectListingView
                     break;
-                case 3:
+                case 4:
                     // MyApplicationView
                     break;
-                case 4:
+                case 5:
                     // BookFlatView
                     break;
-                case 5:
+                case 6:
                     // WithdrawalView
                     break;
-                case 6:
+                case 7:
                     // EnquiriesView
                     break;
-                case 7:
+                case 8:
                     // ProjectOfficerRegistrationView
                     break;
-                case 8:
+                case 9:
                     // OfficerProjectManagementView
 
                 default: System.out.println("Invalid Input");

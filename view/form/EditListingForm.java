8f8388c1058471025f9bc419f14b5f9a8fbd0d53
efilyssem <efilyssem888@gmail.com>
2025-04-25 16:48:22 +0800

    deleted old files and revert EditListingForm

--- view/form/EditListingForm.java
+++ view/form/EditListingForm.java
@@ -1,242 +1,112 @@
 package view.form;
 
-import project.*;
-import project.ProjectRepository;
+import project.Project;
+import project.ProjectController;
+import session.Session;
+import validator.InputValidator;
 import view.FormView;
 
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Scanner;
-import java.util.HashMap;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class EditListingForm extends FormView {
     HashMap<String, Object> userInput = new HashMap<>();
+    private Project oldProject;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public EditListingForm(Project oldProject) {
+        this.oldProject = oldProject;
+    }
 
     @Override
     public void prompt() {
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
         Project project;
         String name = "";
         String neighbourhood = "";
         LocalDate openingDate = null;
         LocalDate closingDate = null;
-        int officerSlots = 0;
-        int total2Units = 0;
-        int available2Units = 0;
-        double price2 = 0;
-        int total3Units = 0;
-        int available3Units = 0;
-        double price3 = 0;
+        int officerSlots = -1;
+        boolean visibility = oldProject.getVisibility();
 
-        System.out.println("Enter project id to be edited: ");
-        String projectId;
-
-        do {
-            projectId = scanner.next();
-            ArrayList<Project> projects = ProjectRepository.getAllProjects();
-            int finalProjectId = Integer.parseInt(projectId);
-            project = projects.stream().filter(p -> p.getProjectId()==(finalProjectId)).findFirst().orElse(null);
-            if (project != null) {
-                System.out.println("Editing Project...");
-            } else {
-                System.out.println("Invalid project name.");
-            }
-        } while (project == null);
-
-        System.out.println("=== Edit BTO Project ===");
+        System.out.println("--- Edit Listing ---");
         System.out.println("Leave field empty to keep current values.");
-        
-        System.out.println("Project name: ");
+
+        System.out.println("Current name - " + oldProject.getProjectName());
+        System.out.println("Name: ");
         name = scanner.next();
 
+        System.out.println("Current neighbourhood - " + oldProject.getNeighbourhood());
         System.out.println("Neighbourhood: ");
         neighbourhood = scanner.next();
 
-        System.out.println("Application opening date (dd-MM-yyyy): ");
-        String sOpeningDate;
-        boolean validOpening = false;
+        System.out.println("Current opening date - " + oldProject.getApplicationOpeningDate());
+        System.out.println("Application Opening Date: ");
+        String sOpening;
         do {
-            sOpeningDate = scanner.next();
-            if (!sOpeningDate.isEmpty()) {
-                try {
-                    openingDate = LocalDate.parse(sOpeningDate, formatter);
-                    validOpening = true;
-                } catch (DateTimeParseException e) {
-                    System.out.println("Invalid date format!");
+            sOpening = scanner.next();
+            if (!sOpening.isEmpty()) {
+                if (InputValidator.validateDate(sOpening, "dd/MM/yyyy")) {
+                    openingDate = LocalDate.parse(sOpening, formatter);
+                } else {
+                    System.out.println("Invalid date format! Enter date in dd/MM/yyyy format.");
                 }
             }
-            else {
-                validOpening = true;
-            }
-        } while (!validOpening);
+        } while (!sOpening.isEmpty() && !InputValidator.validateDate(sOpening, "dd/MM/yyyy"));
 
-        System.out.println("Application closing date (dd-MM-yyyy): ");
-        String sClosingDate;
-        boolean validClosing = false;
+        System.out.println("Current closing date - " + oldProject.getApplicationClosingDate());
+        System.out.println("Application Closing Date: ");
+        String sClosing;
         do {
-            sClosingDate = scanner.next();
-            if (!sClosingDate.isEmpty()) {
-                try {
-                    closingDate = LocalDate.parse(sClosingDate, formatter);
-                    validClosing = true;
-                } catch (DateTimeParseException e) {
-                    System.out.println("Invalid date format!");
+            sClosing = scanner.next();
+            if (!sClosing.isEmpty()){
+                if (InputValidator.validateDate(sClosing, "dd/MM/yyyy")){
+                    closingDate = LocalDate.parse(sClosing, formatter);
+                }
+                else {
+                    System.out.println("Invalid date format! Enter date in dd/MM/yyyy format.");
                 }
             }
-            else {
-                validClosing = true;
-            }
-        } while (!validClosing);
+        } while (!sClosing.isEmpty() && !InputValidator.validateDate(sClosing, "dd/MM/yyyy"));
 
-        System.out.println("Officer slots: ");
-        String sOfficerSlots;
-        boolean validSlots = false;
+        System.out.println("Current number of officer slots - " + oldProject.getOfficerSlots());
+        System.out.println("Number of Officer Slots: ");
+        String sSlots;
         do {
-            sOfficerSlots = scanner.next();
-            if (!sOfficerSlots.isEmpty()) {
-                try {
-                    officerSlots = Integer.parseInt(sOfficerSlots);
-                    validSlots = true;
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid number format!");
+            sSlots = scanner.next();
+            if (!sSlots.isEmpty()) {
+                if (InputValidator.validateIntRange(sSlots, 1, 10)) {
+                    officerSlots = Integer.parseInt(sSlots);
+                }
+                else {
+                    System.out.println("Invalid number format! Enter an integer between 1 and 10");
                 }
             }
-            else {
-                validSlots = true;
+        } while (!sSlots.isEmpty() && !InputValidator.validateIntRange(sSlots, 1, 10));
+
+        System.out.println("Current visibility - " + (oldProject.getVisibility()?"on":"off"));
+        System.out.println("Visibility: ");
+        String sVisibility;
+        do {
+            sVisibility = scanner.next().toUpperCase();
+            if (InputValidator.validateYesNo(sVisibility)) {
+                visibility = sVisibility.equalsIgnoreCase("Y");
             }
-        } while (!validSlots);
+        } while (!InputValidator.validateYesNo(sVisibility));
 
 
-        System.out.println("\n=== Editing Unit Types ===");
-        System.out.println("Leave field empty to keep current values");
+        userInput.put("name", name);
+        userInput.put("neighbourhood", neighbourhood);
+        userInput.put("openingDate", openingDate);
+        userInput.put("closingDate", closingDate);
+        userInput.put("officerSlots", officerSlots);
+        userInput.put("visibility", visibility);
 
-//        System.out.println("--- 2 Room Units ---");
-//        System.out.println("Enter total number of units: ");
-//        String s2Total;
-//        boolean valid2Total = false;
-//        do {
-//            s2Total = scanner.next();
-//            if (!s2Total.isEmpty()) {
-//                try {
-//                    total2Units = Integer.parseInt(s2Total);
-//                    valid2Total = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                valid2Total = true;
-//            }
-//        } while (!valid2Total);
-//
-//        System.out.println("Enter available number of units: ");
-//        String s2Avail;
-//        boolean valid2Avail = false;
-//        do {
-//            s2Avail = scanner.next();
-//            if (!s2Avail.isEmpty()) {
-//                try {
-//                    available2Units = Integer.parseInt(s2Avail);
-//                    valid2Avail = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                valid2Avail = true;
-//            }
-//        } while (!valid2Avail);
-//
-//        System.out.println("Enter price per unit: ");
-//        String sPrice2;
-//        boolean validPrice2 = false;
-//        do {
-//            sPrice2 = scanner.next();
-//            if (!sPrice2.isEmpty()) {
-//                try {
-//                    price2 = Integer.parseInt(sPrice2);
-//                    validPrice2 = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                validPrice2 = true;
-//            }
-//        } while (!validPrice2);
-//
-//        System.out.println("--- 3 Room Units ---");
-//        System.out.println("Enter total number of units: ");
-//        String s3Total;
-//        boolean valid3Total = false;
-//        do {
-//            s3Total = scanner.next();
-//            if (!s3Total.isEmpty()) {
-//                try {
-//                    total3Units = Integer.parseInt(s3Total);
-//                    valid3Total = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                valid3Total = true;
-//            }
-//        } while (!valid3Total);
-//
-//        System.out.println("Enter available number of units: ");
-//        String s3Avail;
-//        boolean valid3Avail = false;
-//        do {
-//            s3Avail = scanner.next();
-//            if (!s3Avail.isEmpty()) {
-//                try {
-//                    available3Units = Integer.parseInt(s3Avail);
-//                    valid3Avail = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                valid3Avail = true;
-//            }
-//        } while (!valid3Avail);
-//
-//        System.out.println("Enter price per unit: ");
-//        String sPrice3;
-//        boolean validPrice3 = false;
-//        do {
-//            sPrice3 = scanner.next();
-//            if (!sPrice3.isEmpty()) {
-//                try {
-//                    price3 = Integer.parseInt(sPrice3);
-//                    validPrice3 = true;
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Invalid number format");
-//                }
-//            } else {
-//                validPrice3 = true;
-//            }
-//        } while (!validPrice3);
-    
-
-
-
-
-//        userInput.put("project", project);
-//        userInput.put("name", name);
-//        userInput.put("neighbourhood", neighbourhood);
-//        userInput.put("openingDate", openingDate);
-//        userInput.put("closingDate", closingDate);
-//        userInput.put("officerSlots", officerSlots);
-//        userInput.put("2-room totalUnits", total2Units);
-//        userInput.put("2-room availableUnits", available2Units);
-//        userInput.put("2-room price", price2);
-//        userInput.put("3-room totalUnits", total3Units);
-//        userInput.put("3-room availableUnits", available3Units);
-//        userInput.put("3-room price", price3);
-//
-//        ProjectController projectController = new ProjectController(userInput);
-//        projectController.editListing();
+        ProjectController.editListingWithUserInput(oldProject, this.getUserInput());
+        //Session.getSession().setCurrentView(new EditUnitForm(oldProject));
 
     }
 
